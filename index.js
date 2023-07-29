459779c2f8d3e0834ff3b49db74febd0e84befad
AklMa7 <fire-2000@hotmail.fr>
2023-07-29 02:19:09 +0100

    Added SignUp functionality / bcryptjs for the pswrd security

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const { MongoDB_USER, MongoDB_PASSWORD, MongoDB_IP, MongoDB_PORT } = require("./
 
 // wiring the router.
 const postRouter = require("./routes/postRoutes");
-
+const userRouter = require("./routes/userRoutes");
 
 
 
 //creates an instance of the Express.js application by invoking the express() function. 
 //The app variable represents the Express.js application.
 const app = express()
+
+
 
 // Connect to database's container via mongoose     27017=default port / A loop with a 5sec interval until DB connects.
 const connectWithRetry = () => {
@@ -29,8 +31,12 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
-// middleware for when we CRUD a post.
+
+
+// middleware for when we do any action.
 app.use(express.json());
+
+
 
 // Route for testing purposes / sets up a route handler for the root URL ("/").
 app.get( "/" , (req , res) => {
@@ -38,12 +44,22 @@ app.get( "/" , (req , res) => {
 });
 
 
+
+
+// URL that we route every action to !!!
 //if a user sends a request "localhost:3000/api/v1/posts" it will be sent to postRouter   ( the /posts will be deleted from URL)
 app.use("/api/v1/posts", postRouter);
+
+app.use("/api/v1/users", userRouter);
+
+
+
 
 
 const port = process.env.PORT || 3000 ; // If env variable PORT is set we assign it to port, if not 3000 is assigned to port const.
 // Can take this to the config file in the config folder
+
+
 
 // Starts the expressJS app and makes it listen for incomin requests on the specific port. 
 // listen func has 2 args : port as first and callback function second that logs a message to console(terminal)
--- models/userModel.js
+++ models/userModel.js
@@ -1,2 +1,17 @@
 const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: [true, "User must input a username !"],
+        unique: true
+    },
+    password: {
+        type: String,
+        require: [true, "User must input a password !"]
+    },   
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
